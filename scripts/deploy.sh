ed7d389302c6c1317e9088b39d161f6e7fdc37b5
daniil.stepyrev <danik-steperev@mail.ru>
2020-05-05 22:44:24 +0300

    trying to deploy on server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,15 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/sweater-london.pem \
-    targer/sweater-1.0-SNAPSHOT.jar \
-    ec2-user@35.178.115.187:/home/ec2-user/
+scp -i ~/.ssh/sweater-server.pem \
+    target/sweater-1.0-SNAPSHOT.jar \
+    ec2-user@ec2-3-10-207-24.eu-west-2.compute.amazonaws.com:/home/ec2-user/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/sweater-london.pem ec2-user@ec2-35-178-115-187.eu-west-2.compute.amazonaws.com << EOF
-
-pgrep java | hargs kill -9
+ssh -i ~/.ssh/sweater-server.pem ec2-user@ec2-3-10-207-24.eu-west-2.compute.amazonaws.com << EOF
+pgrep java | xargs kill -9
 nohup java -jar sweater-1.0.SNAPSHOT.jar > log.txt &
-
 EOF
 
 echo 'Bye'
