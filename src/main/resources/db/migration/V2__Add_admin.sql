1fb61030f03f7254f1a184cec4b68a6c89ddb751
daniil.stepyrev <danik-steperev@mail.ru>
2020-05-06 17:23:55 +0300

    added message editor and message list

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-insert into usr (id, username, password, active)
-    values (1, 'admin', '123', true);
+INSERT INTO usr (id, username, password, active)
+    VALUES (1, 'admin', '123', true);
 
-insert into user_role (user_id, roles)
-    values (1, 'USER'), (1, 'ADMIN');
+INSERT INTO user_role (user_id, roles)
+    VALUES (1, 'USER'), (1, 'ADMIN');
