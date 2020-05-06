fc54c97002d8be6a118028dc5aace76d8c8b3525
daniil.stepyrev <danik-steperev@mail.ru>
2020-05-06 10:24:30 +0300

    added files of editor

--- target/classes/db/migration/V2__Add_admin.sql
+++ target/classes/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-INSERT INTO usr (id, username, password, active)
-    VALUES (1, 'admin', '123', true);
+insert into usr (id, username, password, active)
+    values (1, 'admin', '123', true);
 
-INSERT INTO user_role (user_id, roles)
-    VALUES (1, 'USER'), (1, 'ADMIN');
+insert into user_role (user_id, roles)
+    values (1, 'USER'), (1, 'ADMIN');
